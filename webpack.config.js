229ea77277c168e1acf482cd1efd29d98ac551ac
Victoria Likhuta <v1ktor1a09051994@gmail.com>
2019-01-12 00:25:52 +0300

    fix: fix public path

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js',
-    publicPath: '/dist/',
+    publicPath: '/game/',
   },
   module: {
     rules: [
