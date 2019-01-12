3d0f9e0adfed6eaea9f9fb467ab6f0235a160b08
Victoria Likhuta <v1ktor1a09051994@gmail.com>
2019-01-12 08:44:03 +0300

    Updates

--- src/components/tasks/math/math.js
+++
@@ -1,7 +0,0 @@
-import * as Task from '../task-methods';
-import jsonMath from './math.json';
-
-export default async function drawMathTask() {
-  await Task.draw(jsonMath);
-  Task.addListeners(jsonMath);
-}
--- src/components/tasks/task.js
+++
@@ -1,6 +0,0 @@
-import './task.css';
-import drawMathTask from './math/math';
-
-export default function openTaskScreen(className) {
-  if (className === 'math') drawMathTask();
-}
